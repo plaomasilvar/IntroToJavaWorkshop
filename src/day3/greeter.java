4f02088b9d89fb1fa17a5dd9b3c9a477d6d9dbce
League <Level 2>
2017-03-29 12:02:37 -0700

    the hawking way

---
+++ src/day3/greeter.java
@@ -0,0 +1,32 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+public class greeter {
+
+public static void main(String[] args) {
+System.out.println("early bird gets the worm");
+JOptionPane.showMessageDialog(null, "hello,its me");
+JOptionPane.showMessageDialog(null, "I like your glasses");
+JOptionPane.showMessageDialog(null, " Me gustan tus lentes");
+JOptionPane.showMessageDialog(null, "You are awesome");
+JOptionPane.showMessageDialog(null, "Goodbye");
+
+
+
+}
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
---
+++ src/day3/snooper.java
@@ -0,0 +1,26 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+public class snooper {
+
+public static void main(String[] args) {
+
+String Jackie=JOptionPane.showInputDialog("what's your name?");
+JOptionPane.showInputDialog(null, "Hello"+Jackie);
+String Emily=JOptionPane.showInputDialog(null, "When was the date of your Mother's death?"+Jackie);
+String Jazmin=JOptionPane.showInputDialog(null, "When was your house robbed?");
+JOptionPane.showMessageDialog(null, "what's your name?"+Jackie+ "Hello"+ Jackie+ "When was the date of your mother's death?"+ "When was your house robbed?");
+
+
+
+
+}
+
+
+
+
+
+
+
+}
